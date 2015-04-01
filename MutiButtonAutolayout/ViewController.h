456e6456ec26baf32f36ccc143df800276430dca
yuxiaopeng <yuxiaopeng@breadtrip.com>
2015-04-01 10:59:56 +0800

    Initial Commit

---
+++ MutiButtonAutolayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MutiButtonAutolayout
+//
+//  Created by 于晓鹏 on 15/4/1.
+//  Copyright (c) 2015年 roc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MutiButtonAutolayout/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MutiButtonAutolayout
+//
+//  Created by 于晓鹏 on 15/4/1.
+//  Copyright (c) 2015年 roc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
